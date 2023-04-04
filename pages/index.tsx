897bd6c8b4f72b3801ed540d00b475267c9cad8c
Tickether <tickether@gmail.com>
2023-04-04 12:53:40 +0000

    api + ssr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,93 @@
 import type { NextPage } from 'next'
-//import Head from 'next/head'
-//import Image from 'next/image'
-//import styles from '../styles/Home.module.css'
 import { Web3Button, Web3NetworkSwitch } from '@web3modal/react';
+import { Network, Alchemy } from 'alchemy-sdk';
+import Link from "next/link";
 
 
 
-const Home: NextPage = () => {
+
+
+
+
+export async function getStaticProps() {
+  
+  const settings = {
+    apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY, //"wQhhyq4-jQcPzFRui3PljR6pzRwd5N_n",
+    network: Network.ETH_SEPOLIA,
+  };
+
+  // init with key and chain info 
+  const alchemy = new Alchemy(settings);
+  // Print total NFT collection returned in the response:
+  const loadProducts = await alchemy.nft.getNftsForContract("0x76b41A6b4f4F53F8d15163FBC23b9C2B306b48DA")
+  const loadedProducts = JSON.stringify(loadProducts)
+
+  // Pass data to the page via props
+  return { props: { loadedProducts } }
+}
+
+const Home: NextPage <{ loadedProducts: string }> = ({ loadedProducts }) => {
+  
+  const loadProducts = /* Object.entries */((JSON.parse(loadedProducts)).nfts)
+  console.log(loadProducts)
+  
+  
+
   return (
     <>
       {/* Predefined button  */}
       <Web3Button icon="show" label="Connect Wallet" balance="show" />
-      <br />
-      <p>it is {process.env.NEXT_PUBLIC_W3M_PROJECT_ID}</p>
       {/* Network Switcher Button */}
       <Web3NetworkSwitch />
-      <br />
+      
+      {/* 
+      <div>
+    
+      {loadProducts.map(()=>(
+        
+        <Rentals item={item} key={item.tokenId}/> 
+      ))}
+        
+      </div>
+      */}
+      
     </>
   );
 }
 
 export default Home
+
+
+// unused snippets
+//import { useEffect } from 'react';
+
+//import { SDK, Auth } from '@infura/sdk';
+
+/*
+const auth = new Auth({
+  projectId: process.env.INFURA_API_KEY,
+  secretId: process.env.INFURA_API_KEY_SECRET,
+  privateKey: process.env.WALLET_PRIVATE_KEY,
+  chainId: 1115511
+
+})
+*/
+//const sdk = new SDK(auth);
+
+
+/*
+  useEffect(()=>{
+    async function loadProducts() {
+      const settings = {
+        apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY, //"wQhhyq4-jQcPzFRui3PljR6pzRwd5N_n",
+        network: Network.ETH_SEPOLIA,
+      };
+    
+      const alchemy = new Alchemy(settings);
+      // Print total NFT collection returned in the response:
+      const loadedProducts = await alchemy.nft.getNftsForContract("0x76b41A6b4f4F53F8d15163FBC23b9C2B306b48DA")
+      console.log(loadedProducts)
+    }
+    loadProducts()
+  },[])
+  */
