c468ffd620807b82fedda34065173ec25179c325
Tickether <tickether@gmail.com>
2023-06-01 17:54:22 +0000

    still prod buggy

--- pages/history.tsx
+++ pages/history.tsx
@@ -1,10 +1,4 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
-import { configureChains, createClient, WagmiConfig } from 'wagmi'
-import { sepolia, mainnet, polygon } from 'wagmi/chains'
-import { useEffect, useState } from 'react'
+import '../styles/History.module.css'
 
 
 function HistoryPage() {
